5807f3671f325b82b300226631bbd6b875fa07f4
Johnson Wang <johnsonwang529@gmail.com>
2024-04-27 22:06:34 -0400

    setup

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from db import db
 from flask import Flask,request
 import json
+from db import User, Post, Comment
 
 app = Flask(__name__)
 db_filename = "cms.db"
@@ -20,3 +21,24 @@ def success_response(body, code=200):
 
 def failure_response(message, code=404):
     return json.dumps({"error": message}), code
+
+
+#### User Routes ####
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/api/users/", methods=["POST"])
+def create_user():
+    post_body = json.loads(request.data)
+    username = post_body.get("username", "")
+    password = post_body.get("password", "")
+    if username == "" or password == "":
+        return failure_response("Invalid username or password")
+    user = User(username=username, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return success_response(user.serialize(), 201)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- src/db.py
+++ src/db.py
@@ -1,3 +1,67 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+#### Models ####
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    blogs = db.relationship("Post", cascade="delete")
+    comments = db.relationship("Comment", cascade="delete")
+
+    def __init__(self, **kwargs):
+        self.username = kwargs.get("username", "")
+        self.password = kwargs.get("password", "")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "posts": [p.serialize() for p in self.posts],
+            "comments": [c.serialize() for c in self.comments]
+        }
+        
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    comments = db.relationship("Comment", cascade="delete")
+
+    def __init__(self, **kwargs):
+        self.title = kwargs.get("title", "")
+        self.content = kwargs.get("content", "")
+        self.user_id = kwargs.get("user_id", 0)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "user_id": self.user_id,
+            "comments": [c.serialize() for c in self.comments]
+        }
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.content = kwargs.get("content", "")
+        self.user_id = kwargs.get("user_id", 0)
+        self.post_id = kwargs.get("post_id", 0)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+        }
+
